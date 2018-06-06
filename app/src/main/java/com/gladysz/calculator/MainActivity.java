b919f10449dab3c548478a7bb8465b8a8bcbe203
Marcin <rafael.gladysz@wp.pl>
2018-06-06 23:54:54 +0200

    basic view

--- app/src/main/java/com/gladysz/calculator/MainActivity.java
+++ app/src/main/java/com/gladysz/calculator/MainActivity.java
@@ -2,12 +2,27 @@ package com.gladysz.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button nine;
+    private Button eight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUIViews();
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        nine = findViewById(R.id.btn9);
+        eight = findViewById(R.id.btn8);
     }
 }
